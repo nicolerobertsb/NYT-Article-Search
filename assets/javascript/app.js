fc2cb724d0cf90b208b6df9a5568bddf26f8f208
danlgrigg <griggdl@gmail.com>
2019-09-09 13:44:53 -0700

    Created click function and declared variables to collect user input.  Created reset function to clear input fields.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
-//create click event for submit button and clear button
-//set variables for each id and 
-// grab user input from from search fields
-    //id = search-term
-    //id = number-of-records
-    //id = start-year
-    //id = end-year
-    //id = search-button
-    //id = clear-results-button
+//create click event for search button and clear button
+$("#search-button").on("click", function(event) {
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+    //set variables for each id 
+    // grab user input from from search fields
+    var searchTerm= $("#search-term").val();
+    var numOfRecords= $("#number-of-records").val();
+    var startYear = $("#start-year").val();
+    var endYear = $("#end-year").val()
+    var searchButton = $("#search-button").val()
+    var clearResultsButton = $("#clear-results-button").val()
+    //send values to ajax script
+});
+
     
-//create 
+$("#clear-results-button").on("click", function(event) {
+    myForm.reset()
+
+});
+
