cffa12f0621e1c3354a16d2097c031d722fbece4
danlgrigg <griggdl@gmail.com>
2019-09-09 12:40:00 -0700

    Added pseudocode to app.js, listed ids for elements.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//create click event for submit button and clear button
+//set variables for each id and 
+// grab user input from from search fields
+    //id = search-term
+    //id = number-of-records
+    //id = start-year
+    //id = end-year
+    //id = search-button
+    //id = clear-results-button
+    
+//create 
